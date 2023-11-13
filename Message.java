e154895bfcd776750d880ae5a1d7e79117c4339c
Dinesh M K <150262225+DineshMKJuniorDevloper@users.noreply.github.com>
2023-11-13 04:27:14 -0800

    Add files via upload

---
+++ Message.java
@@ -0,0 +1,17 @@
+package com.example.Model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Message {
+
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+}
---
+++ Status.java
@@ -0,0 +1,7 @@
+package com.example.Model;
+
+public enum Status {
+    JION,
+    Message,
+    LEAVE
+}
